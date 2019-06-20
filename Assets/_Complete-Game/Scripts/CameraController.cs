b25b63396629ec2c695e5eabf31a9d3619dc855e
Nirvana_Chen <xinxrong@foxmail.com>
2019-06-20 21:21:28 +0800

    v1.0.1release

--- Assets/_Complete-Game/Scripts/CameraController.cs
+++ Assets/_Complete-Game/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
 
     private Transform target; //角色的Transform
-    public Vector3 Offset = new Vector3(7.5f, 3.5f, -10f);  //摄像机与角色的相对位置偏移
+    public Vector3 Offset = new Vector3(5.5f, 3.5f, -10f);  //摄像机与角色的相对位置偏移
     private float smoothing = 3; //移动平滑度
                                  // Use this for initialization
     void Start()
@@ -20,7 +20,7 @@ public class CameraController : MonoBehaviour
     /// </summary>
     void LateUpdate()
     {
-        if (target.position.x <= 8)
+        if (target.position.x <= 13)
         {
             //target.TransformDirection(Offset) 将偏移从局部坐标变为世界坐标,达到摄像机永远在角色背后的目的
             transform.position = new Vector3(Vector3.Lerp(transform.position, target.position + target.TransformDirection(Offset), Time.deltaTime * smoothing).x, 3.5f, -10f);
